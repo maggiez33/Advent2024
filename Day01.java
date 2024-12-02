020eb96c0e826eaf531bf156992b1aa8cc08f431
maggiez33 <145692117+maggiez33@users.noreply.github.com>
2024-12-02 14:49:29 -0500

    Add files via upload

--- Day01.java
+++ Day01.java
@@ -2,6 +2,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day01 {
@@ -17,22 +19,14 @@ public class Day01 {
             right.add(fileData.get(i).split("   ")[1]);
         }
 
+        Collections.sort(left);
+        Collections.sort(right);
 
-        for (int i = 1; i < left.size(); i++) {
-            int minLeft = Integer.parseInt(left.get(i));
-            for (int j = i; j < left.size(); j++) {
-                if (Integer.parseInt(left.get(i - 1)) < minLeft) {
-                    minLeft = Integer.parseInt(left.get(i - 1));
-                }
-            }
+        for (int i = 0; i < left.size(); i++) {
 
-            int minRight = Integer.parseInt(left.get(i));
-            for (int j = i; j < right.size(); j++) {
-                if (Integer.parseInt(right.get(i - 1)) < minRight) {
-                    minLeft = Integer.parseInt(right.get(i - 1));
-                }
-            }
+            total += Math.abs(Integer.parseInt(left.get(i)) - Integer.parseInt(right.get(i)));
         }
+        System.out.println(total);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
