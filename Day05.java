4f670159fa67a15d6595689bcf8f7fdc2f34041c
maggiez33 <145692117+maggiez33@users.noreply.github.com>
2024-12-11 14:50:59 -0500

    Add files via upload

--- Day05.java
+++ Day05.java
@@ -13,18 +13,48 @@ public class Day05 {
 
         ArrayList<String> fileData = getFileData("src/Day5Input.txt");
         System.out.println(fileData.size());
-
-        ArrayList<String> rules = new ArrayList<String>();
+        ArrayList<String> ordered = new ArrayList<>();
+        ArrayList<String> rules = new ArrayList<>();
+        ArrayList<String> pages = new ArrayList<>();
         for (int i = 0; i < 1176; i++) {
             String[] r = fileData.get(i).split("\\|");
             rules.add(Arrays.toString(r));
         }
         for (int i = 1176; i < fileData.size(); i++) {
-            
+            String[] p = fileData.get(i).split(",");
+            pages.add(Arrays.toString(p));
         }
+
+        for (int i = 0; i < pages.size(); i++) {
+            System.out.println(pages.get(i).);
+            /*
+            boolean correct = true;
+            for (int j = 0; j < rules.size(); j++) {
+                for (int pageidx = 0; pageidx < pages.get(i).length(); pageidx++) {
+
+                }
+            }
+            if (correct) {
+                ordered.add(pages.get(i));
+            }
+            */
+        }
+        /*
+        int sum = 0;
+        for (int i = 0; i < ordered.size(); i++) {
+
+        }
+
+        System.out.println(sum);
+
+         */
+        System.out.println(rules.get(0));
+        System.out.println(rules.getLast());
+        System.out.println(pages.get(0));
+        System.out.println(pages.getLast());
         System.out.println(fileData.get(1175));
     }
-
+    public String[] applyRule
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
